9452c350229cfc775f9694976ad615e8f57d8a19
Ayami ONAKA <ayami@sforzando.co.jp>
2018-07-10 03:06:43 +0900

    2. Python インタプリタを使う

---
+++ 002/tutorial_2_2_1.py
@@ -0,0 +1,9 @@
+# -*- coding: encoding -*- 
+# Pythonがcodesでサポートされているエンコーディングを指定
+
+# -*- coding: cp-1252 -*-
+# Windows-1252エンコーディングを指定
+
+#!/usr/bin/env python3
+# -*- coding: cp-1252 -*-
+# "shebang"行で始まる場合は2行目に記述
