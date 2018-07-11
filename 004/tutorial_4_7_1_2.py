10c3136a98bf55f417f12c96060f43b039d86e34
Ayami ONAKA <ayami@sforzando.co.jp>
2018-07-11 23:32:37 +0900

    4.7.2. キーワード引数

--- 004/tutorial_4_7_1_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-i = 5
-
-def f(arg=i):
-    print(arg)
-
-i = 6
-f()
--- 004/tutorial_4_7_1_3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def f(a, L=[]):
-    L.append(a)
-    return L
-
-print(f(1))
-print(f(2))
-print(f(3))
