7ac3362b386edaf0deb40e108b9bf816751d291e
Ayami ONAKA <ayami@sforzando.co.jp>
2018-07-14 10:44:26 +0900

    9.1. 名前とオブジェクトについて

---
+++ 008/tutorial_8_3_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+    except OSError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'lines')
+        f.close()
+        
