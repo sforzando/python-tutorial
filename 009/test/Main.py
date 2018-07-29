84ad92aca765a1bc549ba50218d5775f519843ae
Ayami ONAKA <ayami@sforzando.co.jp>
2018-07-30 04:07:26 +0900

    test_Inheritance_super

--- 009/test/Main.py
+++ 009/test/Main.py
@@ -2,20 +2,25 @@
 
 from Teacher import Teacher
 from Student import Student
+from SuperMan import SuperMan
 
 class Main():
     def __init__(self):
         self.t = Teacher(3)
         self.s = Student(5)
+        self.sm = SuperMan(7)
+        super(Main, self).__init__()
         
 
     def get_number(self):
         print('Teacher: {}'.format(self.t.get_number()))
         print('Student: {}'.format(self.s.get_number()))
+        print('SuperMan: {}'.format(self.sm.get_number()))
 
     def set_student_number(self):
-        self.t.number = 2
-        # self.s.set_number(9)
+        self.t.set_number(2)
+        self.s.set_number(4)
+        self.sm.set_number(6)
 
 if __name__ == '__main__':
     main = Main()
---
+++ 009/test/SuperMan.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class SuperMan(object):
+    def __init__(self, number):
+        self.number = 9
+
+    def get_number(self):
+        return self.number
+
+    def set_number(self, number):
+        self.number = 9
