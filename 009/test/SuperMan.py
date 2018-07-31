211ae983178a4a66a1270b2b051e028dcd1e30a8
Ayami ONAKA <ayami@sforzando.co.jp>
2018-07-31 18:26:35 +0900

    test_Inheritance

--- 009/test/SuperMan.py
+++ 009/test/SuperMan.py
@@ -3,10 +3,7 @@ from Human import Human
 
 class SuperMan(Human):
     def __init__(self, number):
-        self.__number = 9
-
-    def get_number(self):
-        return self.__number
-
+        super().__init__(number = 9)
+        
     def set_number(self, number):
-        self.__number = 9
+        pass
